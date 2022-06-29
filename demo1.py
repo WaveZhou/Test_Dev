653d0b380efd1ab09a454dbf58d528ce3bcf934d
WaveZhou <myqq@qq.com>
2022-06-29 17:21:50 +0800

    第5次提交 纯属测试

--- demo1.py
+++ demo1.py
@@ -1,7 +1,8 @@
 from WindPy import w
 
-w.start() # 默认命令超时时间为120秒，如需设置超时时间可以加入waitTime参数，例如waitTime=60,即设置命令超时时间为60秒
+w.start()  # 默认命令超时时间为120秒，如需设置超时时间可以加入waitTime参数，例如waitTime=60,即设置命令超时时间为60秒
 
-w.isconnected() # 判断WindPy是否已经登录成功
-what = w.wsd('600030.SH','CLOSE','2016-01-01','2016-01-05','')
+w.isconnected()  # 判断WindPy是否已经登录成功
+what = w.wsd('600030.SH', 'CLOSE', '2016-01-01', '2016-01-05', '')
 print(what)
+# 我随便在开发分支里写点东西
